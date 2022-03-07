ec0cc503583e4dcd67d963c1c331d8e34734e86f
Federico <fluna1973@yahoo.com>
2022-03-06 22:36:20 -0600

    /js/recipe_book.js

--- js/recipe_book.js
+++ js/recipe_book.js
@@ -1,9 +1,2 @@
 alert("hello world");
 
-
-let age = prompt('age?', 18);
-
-let message = (age < 3) ? 'Hi, baby!' :
-  (age < 18) ? 'Hello!' :
-  (age < 100) ? 'Greetings!' :
-  'What an unusual age!';
